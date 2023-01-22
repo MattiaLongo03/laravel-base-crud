b03a610f791c519d6e496c24ed873915f7b7a30e
MattiaLongo03 <mattilongo2003@gmail.com>
2023-01-22 18:45:46 +0100

    Creazione Database con Seeder e Model

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Comic;
+use Doctrine\DBAL\Tools\Console\Command\ImportCommand;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
